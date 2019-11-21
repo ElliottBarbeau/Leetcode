e609a034421d25fdd46248ce180bd1b418239055
ElliottBarbeau <elliottbarbeau@gmail.com>
2019-11-20 19:14:08 -0500

    DFS
    recursive tree traversal dfs algorithms

---
+++ Depth First Search.py
@@ -0,0 +1,44 @@
+#Recursive inorder traversal
+class Solution:
+    def inorderTraversal(self, root):
+        if not root:
+            return []
+        l = []
+        def dfs(node):
+            if node.left:
+                dfs(node.left)
+            l.append(node.val)
+            if node.right:
+                dfs(node.right)
+            return l
+        return dfs(root)
+
+#Recursive preorder traversal
+class Solution2:
+    def preorderTraversal(self, root):
+        if not root:
+            return []
+        l = []
+        def dfs(node):
+            l.append(node.val)
+            if node.left:
+                dfs(node.left)
+            if node.right:
+                dfs(node.right)
+            return l
+        return dfs(root)
+
+#Recursive postorder traversal
+class Solution3:
+    def postorderTraversal(self, root):
+        if not root:
+            return []
+        l = []
+        def dfs(node):
+            if node.left:
+                dfs(node.left)
+            if node.right:
+                dfs(node.right)
+            l.append(node.val)
+            return l
+        return dfs(root)
