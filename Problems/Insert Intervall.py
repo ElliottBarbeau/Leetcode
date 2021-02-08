5952ba50e6b9dc514154707b01e2ffd547683293
ElliottBarbeau <elliottbarbeau@gmail.com>
2021-02-07 22:44:32 -0500

    Insert Interval done without any help

---
+++ Problems/Insert Intervall.py
@@ -0,0 +1,32 @@
+class Solution:
+    def insert(self, intervals, newInterval):
+        intervals.sort()
+        if not intervals:
+            return [newInterval]
+        res = []
+        i = 0
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+
+        start = newInterval[0]
+        end = newInterval[1]
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+            start = min(intervals[i][0], start)
+            end = max(intervals[i][1], end)
+            i += 1
+
+        res.append([start, end])
+
+        while i < len(intervals) and intervals[i][0] > newInterval[1]:
+            res.append(intervals[i])
+            i += 1
+
+        return res
+
+intervals = [[1,3],[6,9]]
+newInterval = [2,5]
+print(Solution().insert(intervals, newInterval))
+intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
+newInterval = [4,8]
+print(Solution().insert(intervals, newInterval))
